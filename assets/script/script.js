157a650cc146f76e4b4447f4318120356db47442
Liem Nguyen <liemnguyen0111@gmail.com>
2020-04-21 01:49:46 -0700

    Added new files

---
+++ assets/script/script.js
@@ -0,0 +1,10 @@
+  let prevScrollpos = window.pageYOffset
+    window.onscroll = function () {
+      let currentScrollPos = window.pageYOffset
+      if (prevScrollpos > currentScrollPos) {
+    document.getElementById("myNav").style.top = "0"
+      } else {
+        document.getElementById("myNav").style.top = '-' + document.getElementById("myNav").clientHeight + 'px'
+      }
+      prevScrollpos = currentScrollPos
+    }
