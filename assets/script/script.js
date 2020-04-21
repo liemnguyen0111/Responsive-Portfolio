aa6461237a51232dd3e69372f44825a0e1ee2bf4
Liem Nguyen <liemnguyen0111@gmail.com>
2020-04-21 02:07:04 -0700

    Updater script.js

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,7 +1,7 @@
   let prevScrollpos = window.pageYOffset
     window.onscroll = function () {
       let currentScrollPos = window.pageYOffset
-      if (prevScrollpos > currentScrollPos) {
+      if (prevScrollpos > currentScrollPos || document.getElementById("wrapper").style.top === "0") {
     document.getElementById("myNav").style.top = "0"
       } else {
         document.getElementById("myNav").style.top = '-' + document.getElementById("myNav").clientHeight + 'px'
