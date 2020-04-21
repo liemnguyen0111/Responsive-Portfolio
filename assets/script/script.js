803b8ba40d7f7b08fc68b42aae566cf0862dd0c7
Liem Nguyen <liemnguyen0111@gmail.com>
2020-04-21 02:10:19 -0700

    Updated script.js

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,7 +1,7 @@
   let prevScrollpos = window.pageYOffset
     window.onscroll = function () {
       let currentScrollPos = window.pageYOffset
-      if (prevScrollpos > currentScrollPos || document.getElementById("wrapper").style.top === "0") {
+      if (prevScrollpos > currentScrollPos || document.getElementById("wrapper").clientTop === "0") {
     document.getElementById("myNav").style.top = "0"
       } else {
         document.getElementById("myNav").style.top = '-' + document.getElementById("myNav").clientHeight + 'px'
